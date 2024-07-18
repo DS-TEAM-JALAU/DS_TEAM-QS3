3e5f25e5d21330ad8ce40825120e8f14c02d7cd5
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2024-07-18 15:19:22 -0300

    test: should return status code 400 if an invalid url was provided

--- tests/willian.spec.js
+++ tests/willian.spec.js
@@ -1,19 +1,22 @@
 import axios from "axios";
 
-const makeRequest = async () => {
-  try {
-    const response = await axios.get(
-      "https://viacep.com.br/ws/RS/Porto Alegre/Dom/json/"
-    );
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
-};
-
 describe("Willian", () => {
   test("should return a limit of 50 ceps", async () => {
-    const response = await makeRequest();
-    expect(response.length).toBe(50);
+    const { data } = await axios.get(
+      "https://viacep.com.br/ws/RS/Porto Alegre/Dom/json/"
+    );
+    expect(data.length).toBe(50);
+  });
+
+  test("should return status code 400 if an invalid url was provided", async () => {
+    let statusCode;
+
+    await axios
+      .get("https://viacep.com.br/ws/RS/Porto Alegre/Domingos/jso/")
+      .catch((err) => {
+        statusCode = err.response.status;
+      });
+
+    expect(statusCode).toBe(400);
   });
 });
