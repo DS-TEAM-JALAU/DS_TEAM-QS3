b7b47f3f2fa3d492065ace75719654bd677bc9c1
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2024-07-18 15:08:57 -0300

    test: should return a limit of 50 ceps

--- tests/willian.spec.js
+++ tests/willian.spec.js
@@ -1,7 +1,19 @@
 import axios from "axios";
 
-const makeRequest = () => {};
+const makeRequest = async () => {
+  try {
+    const response = await axios.get(
+      "https://viacep.com.br/ws/RS/Porto Alegre/Dom/json/"
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
 
 describe("Willian", () => {
-  test("should ", async () => {});
+  test("should return a limit of 50 ceps", async () => {
+    const response = await makeRequest();
+    expect(response.length).toBe(50);
+  });
 });
