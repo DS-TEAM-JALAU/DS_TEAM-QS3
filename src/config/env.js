6eb078f43448db2d4a2392e0a4212236ae22347e
IureLima <iurelimasilva@gmail.com>
2024-07-25 13:50:38 -0300

    refactor: modify the env variable

--- src/config/env.js
+++ src/config/env.js
@@ -3,9 +3,5 @@ import { config } from "dotenv";
 config();
 
 export default {
-  URL1: process.env.URL1,
-  URL2: process.env.URL2,
-  URL3: process.env.URL3,
-  URL4: process.env.URL4,
-  URL5: process.env.URL5,
+  URL: process.env.URL,
 };
