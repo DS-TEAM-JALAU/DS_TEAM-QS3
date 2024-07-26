a6847d53a3becba40edebe3b55cd02dc0aa026fa
Roger Rocha <roger.alves@jala.university>
2024-07-26 17:09:01 -0300

    add: request-manager with singleton structure

---
+++ src/request/request-manager.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+class AxiosSingleton {
+  constructor() {
+    if (!AxiosSingleton.instance) {
+      this.axiosInstance = axios.create({
+        baseURL: 'https://viacep.com/'
+    });
+
+      AxiosSingleton.instance = this;
+    }
+
+  }
+
+  getInstance() {
+    return this.axiosInstance;
+  }
+}
+
+const instance = new AxiosSingleton();
+Object.freeze(instance);
+
+export default instance;
