fb02939b9aa8761a0d7dc0f0df5681218b3bcaff
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2024-08-01 13:27:11 -0300

    refactor: remove some spaces

--- tests/willian.spec.js
+++ tests/willian.spec.js
@@ -6,26 +6,28 @@ jest.setTimeout(70 * 3000);
 let axiosInstance;
 beforeAll(() => {
   axiosInstance = AxiosSingleton.getInstance();
-  logger.info("Set Request Manager Instance in the Willian Test")
+  logger.info("Set Request Manager Instance in the Willian Test");
 });
 
 describe("Willian", () => {
   test("should return a limit of 50 ceps", async () => {
-    logger.info("Running should return a limit of 50 ceps")
+    logger.info("Running should return a limit of 50 ceps");
     const { data } = await axiosInstance.get(`/ws/RS/Porto Alegre/Dom/json/`);
     expect(data.length).toBe(50);
-    
   });
 
   test("should return status code 400 if an invalid url was provided", async () => {
-    logger.info("Running should return status code 400 if an invalid url was provided")
+    logger.info(
+      "Running should return status code 400 if an invalid url was provided"
+    );
     let statusCode;
 
-    await axiosInstance.get(`/ws/RS/Porto Alegre/Domingos/jso/`).catch((err) => {
-      statusCode = err.response.status;
-    });
+    await axiosInstance
+      .get(`/ws/RS/Porto Alegre/Domingos/jso/`)
+      .catch((err) => {
+        statusCode = err.response.status;
+      });
 
     expect(statusCode).toBe(400);
-    
   });
 });
