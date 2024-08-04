34c7c1e8d4bf2dfb2dde2c04b4549e75fcd7715b
IureLima <iure.lima@jala.university>
2024-08-03 22:20:15 -0300

    test: modify the test for invalid cidade

---
+++ tests/invalid-cidade.spec.js
@@ -0,0 +1,35 @@
+import { AxiosSingleton } from "../src/request/request-manager.js";
+import { logger } from "../src/config/logger.js";
+
+jest.setTimeout(30000);
+
+
+
+let axiosInstance;
+beforeAll(() => {
+  axiosInstance = AxiosSingleton.getInstance();
+  logger.info("Set Request Manager Instance in the Invalid Cidade Test")
+});
+
+describe(" Invalid Cidade", () => {
+
+  test("should return status bad request a invalid 'cidade' was provided", async () => {
+    logger.info("Running should return status bad request a invalid 'cidade' was provided")
+    let status;
+    await axiosInstance.get(`/ws/RS/Po/Domingos/json/`).catch((err) => {
+      status = err.response.status;
+    });
+
+    expect(status).toBe(400);
+  });
+
+  test("should return status text bad request a invalid 'cidade' was provided", async () => {
+    logger.info("Running should return status text bad request a invalid 'cidade' was provided")
+    let statusText;
+    await axiosInstance.get(`/ws/RS/Po/Domingos/json/`).catch((err) => {
+      statusText = err.response.statusText;
+    });
+
+    expect(statusText).toBe("Bad Request");
+  });
+});
