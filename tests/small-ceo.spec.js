891fd35c2227c81f4e1ccf3c7b674670dd45ef40
IureLima <iure.lima@jala.university>
2024-08-03 22:07:46 -0300

    test: modify the test for small test

---
+++ tests/small-ceo.spec.js
@@ -0,0 +1,30 @@
+import { AxiosSingleton } from "../src/request/request-manager.js";
+import { logger } from "../src/config/logger.js";
+
+
+jest.setTimeout(30000);
+
+let axiosInstance;
+beforeAll(() => {
+  axiosInstance = AxiosSingleton.getInstance();
+  logger.info("Set Request Manager Instance in Small Cep test")
+});
+
+describe("Small Cep", () => {
+  test("should return status code 400", async () => {
+    logger.info("Running should return status code 400")
+    let status;
+    await axiosInstance.get(`/ws/0100100/json/`).catch((err) => {
+      status = err.response.status;
+    });
+    expect(status).toBe(400);
+  });
+  test("should return status text Bad Request", async () => {
+    logger.info("Running should return status text Bad Request")
+    let statusText;
+    await axiosInstance.get(`/ws/0100100/json/`).catch((err) => {
+      statusText = err.response.statusText;
+    });
+    expect(statusText).toBe("Bad Request");
+  });
+});
