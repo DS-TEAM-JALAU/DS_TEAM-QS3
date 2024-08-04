78fe9b62e154252f03ff5365a65474db0db14db1
IureLima <iure.lima@jala.university>
2024-08-03 22:15:28 -0300

    test: modify the test for Logradouro with plus

---
+++ tests/logradouro-with-plus.spec.js
@@ -0,0 +1,41 @@
+import { AxiosSingleton } from "../src/request/request-manager.js";
+import { logger } from "../src/config/logger.js";
+
+jest.setTimeout(30000);
+
+let axiosInstance;
+beforeAll(() => {
+  axiosInstance = AxiosSingleton.getInstance();
+  logger.info("Set Request Manager Instance in the Logradouro with plus Test")
+});
+
+describe("Logradouro with plus", () => {
+  test("should return valid response when use + in 'logradouro'", async () => {
+    logger.info("Running should return valid response when use + in 'logradouro")
+    const { data } = await axiosInstance.get(
+      `/ws/RS/Porto Alegre/Domingos+Jose/json/`
+    );
+
+    expect(data.length).toStrictEqual(2);
+  });
+
+  test("should return status code 200", async () => {
+    logger.info("Running should return status code 200")
+    let statusCode;
+    await axiosInstance.get(
+      `/ws/RS/Porto Alegre/Domingos+Jose/json/`
+    ).then((response) => statusCode = response.status);
+
+    expect(statusCode).toStrictEqual(200);
+  });
+
+  test("should return status text ok", async () => {
+    logger.info("Running should return status text ok0")
+    let statusText;
+    await axiosInstance.get(
+      `/ws/RS/Porto Alegre/Domingos+Jose/json/`
+    ).then((response) => statusText = response.statusText);
+
+    expect(statusText).toStrictEqual("OK");
+  });
+});
