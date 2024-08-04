acb61e9ed684a4672602c8adc3ee04b8154da971
IureLima <iure.lima@jala.university>
2024-08-03 22:06:19 -0300

    refactor: modify the test name

--- tests/big-cep.spec.js
+++ tests/big-cep.spec.js
@@ -10,7 +10,7 @@ beforeAll(() => {
   logger.info("Set Request Manager Instance in Big Cep test")
 });
 
-describe("Bif Cep", () => {
+describe("Big Cep", () => {
   test("should return status code 400", async () => {
     logger.info("Running should return status code 400")
     let status;
