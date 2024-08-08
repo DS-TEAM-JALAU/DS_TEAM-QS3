295b3ff9ea5a45af0a39cfe235e2054751c1b4ae
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2024-08-08 15:51:34 -0300

    test: should return a valid list of cep

--- src/request/request-manager.js
+++ src/request/request-manager.js
@@ -2,19 +2,19 @@ import axios from "axios";
 import env from "../config/env.js";
 
 export class AxiosSingleton {
-  static instance
+	static instance;
 
-  static getInstance() {
-    if (!this.instance) {
-      this.instance = axios.create({
-        baseURL: env.URL,
-        timeout: 10000,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-    }
+	static getInstance() {
+		if (!this.instance) {
+			this.instance = axios.create({
+				baseURL: env.URL,
+				timeout: 10000,
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
+		}
 
-    return this.instance
-  }
+		return this.instance;
+	}
 }
